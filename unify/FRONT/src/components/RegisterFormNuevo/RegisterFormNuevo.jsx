75565a057e4e756310b8a1c37f263325483dccd3
ivaneconomo <ivan.economo39@gmail.com>
2023-04-30 12:57:01 -0300

    update registerForm

--- unify/FRONT/src/components/RegisterFormNuevo/RegisterFormNuevo.jsx
+++ unify/FRONT/src/components/RegisterFormNuevo/RegisterFormNuevo.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import { passwordCheckValidation } from '../../utils/validations';
 import { customAlert, messages } from '../../utils/alerts';
 import { useNavigate } from 'react-router-dom';
@@ -63,18 +65,18 @@ export default function RegisterFormNuevo() {
   };
 
   return (
-    <div className='vh-100 border border-danger border-3 rounded p-1'>
-      body
-      <div className='container border border-warning border-3 rounded p-1'>
-        container
-        <div className='row justify-content-center border border-primary border-3 rounded p-1'>
-          row
-          <section className='col-6 border border-success border-3 rounded p-1'>
-            col
-            <form onSubmit={handleSubmit(onSubmit)} className='border'>
-              <div>
-                <label>Nombre</label>
-                <input
+    <Container>
+      <Row className='justify-content-center'>
+        <Col xs={12} sm={12} md={9} lg={6}>
+          <Form
+            onSubmit={handleSubmit(onSubmit)}
+            className='d-flex flex-column gap-2 m-5 border rounded shadow p-3'
+          >
+            <h2 className='border-bottom pb-3'>Registro</h2>
+            <div className='d-flex gap-2'>
+              <Form.Group>
+                <Form.Label>Nombre</Form.Label>
+                <Form.Control
                   type='text'
                   {...register('name', {
                     required: {
@@ -96,12 +98,14 @@ export default function RegisterFormNuevo() {
                     },
                   })}
                 />
-                {errors.name && <p>{errors.name.message}</p>}
-              </div>
+                {errors.name && (
+                  <small className='text-danger'>{errors.name.message}</small>
+                )}
+              </Form.Group>
 
-              <div>
-                <label>Apellido</label>
-                <input
+              <Form.Group>
+                <Form.Label>Apellido</Form.Label>
+                <Form.Control
                   type='text'
                   {...register('lastName', {
                     required: {
@@ -123,110 +127,139 @@ export default function RegisterFormNuevo() {
                     },
                   })}
                 />
-                {errors.lastName && <p>{errors.lastName.message}</p>}
-              </div>
+                {errors.lastName && (
+                  <small className='text-danger'>
+                    {errors.lastName.message}
+                  </small>
+                )}
+              </Form.Group>
+            </div>
 
-              <div>
-                <label>Email</label>
-                <input
-                  type='email'
-                  {...register('email', {
-                    required: {
-                      value: true,
-                      message: 'El email es obligatorio.',
-                    },
-                    minLength: {
-                      value: 5,
-                      message: 'Demasiado corto...',
-                    },
-                    maxLength: {
-                      value: 100,
-                      message: 'Máximo 100 caracteres.',
-                    },
-                    pattern: {
-                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                      message:
-                        'El correo electrónico debe tener un formato válido.',
-                    },
-                  })}
-                />
-                {errors.email && <p>{errors.email.message}</p>}
-              </div>
+            <Form.Group>
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type='email'
+                {...register('email', {
+                  required: {
+                    value: true,
+                    message: 'El email es obligatorio.',
+                  },
+                  minLength: {
+                    value: 5,
+                    message: 'Demasiado corto...',
+                  },
+                  maxLength: {
+                    value: 100,
+                    message: 'Máximo 100 caracteres.',
+                  },
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message:
+                      'El correo electrónico debe tener un formato válido.',
+                  },
+                })}
+              />
+              {errors.email && (
+                <small className='text-danger'>{errors.email.message}</small>
+              )}
+            </Form.Group>
 
-              <div>
-                <label>Contraseña</label>
-                <input
-                  type={showPassword ? 'text' : 'password'}
-                  {...register('password', {
-                    required: {
-                      value: true,
-                      message: 'La contraseña es obligatoria.',
-                    },
-                    minLength: {
-                      value: 8,
-                      message: 'Mínimo 8 caracteres.',
-                    },
-                    maxLength: {
-                      value: 50,
-                      message: 'Máximo 50 caracteres.',
-                    },
-                    pattern: {
-                      value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,50}$/,
-                      message: 'La contraseña debe tener letras y números.',
-                    },
-                  })}
-                />
-                {errors.password && <p>{errors.password.message}</p>}
-              </div>
+            <div className='d-flex gap-2'>
+              <Form.Group>
+                <Form.Label>Contraseña</Form.Label>
+                <div className='d-flex'>
+                  <Form.Control
+                    type={showPassword ? 'text' : 'password'}
+                    {...register('password', {
+                      required: {
+                        value: true,
+                        message: 'La contraseña es obligatoria.',
+                      },
+                      minLength: {
+                        value: 8,
+                        message: 'Mínimo 8 caracteres.',
+                      },
+                      maxLength: {
+                        value: 50,
+                        message: 'Máximo 50 caracteres.',
+                      },
+                      pattern: {
+                        value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,50}$/,
+                        message: 'La contraseña debe tener letras y números.',
+                      },
+                    })}
+                  />
+                </div>
+                {errors.password && (
+                  <small className='text-danger'>
+                    {errors.password.message}
+                  </small>
+                )}
+              </Form.Group>
 
-              <div>
-                <button
-                  type='button'
-                  onClick={() => setShowPassword(!showPassword)}
-                >
-                  {showPassword ? (
-                    <i class='bi bi-eye-slash'></i>
-                  ) : (
-                    <i class='bi bi-eye'></i>
-                  )}
-                </button>
-              </div>
+              <Form.Group>
+                <Form.Label>Repetir contraseña</Form.Label>
+                <div className='d-flex'>
+                  <Form.Control
+                    className='me-0'
+                    type={showPassword ? 'text' : 'password'}
+                    {...register('passwordCheck', {
+                      required: {
+                        value: true,
+                        message: 'La contraseña es obligatoria.',
+                      },
+                      maxLength: {
+                        value: 50,
+                        message: 'Máximo 50 caracteres.',
+                      },
+                      validate: (value) =>
+                        passwordCheckValidation(password, value),
+                    })}
+                  />
+                  <a
+                    className='my-auto mx-2'
+                    onClick={() => setShowPassword(!showPassword)}
+                  >
+                    {showPassword ? (
+                      <i className='bi bi-eye-slash'></i>
+                    ) : (
+                      <i className='bi bi-eye'></i>
+                    )}
+                  </a>
+                </div>
+                {errors.passwordCheck && (
+                  <small className='text-danger'>
+                    {errors.passwordCheck.message}
+                  </small>
+                )}
+              </Form.Group>
+            </div>
 
-              <div>
-                <label>Repetir contraseña</label>
-                <input
-                  type={showPassword ? 'text' : 'password'}
-                  {...register('passwordCheck', {
-                    required: {
-                      value: true,
-                      message: 'La contraseña es obligatoria.',
-                    },
-                    maxLength: {
-                      value: 50,
-                      message: 'Máximo 50 caracteres.',
-                    },
-                    validate: (value) =>
-                      passwordCheckValidation(password, value),
-                  })}
-                />
-                {errors.passwordCheck && <p>{errors.passwordCheck.message}</p>}
-              </div>
+            <div className='container'>
+              <div className='row justify-content-between align-items-center'>
+                <Form.Group className='col-5 p-0'>
+                  <Form.Label>
+                    Idioma <small className='text-secondary'>(opcional)</small>
+                  </Form.Label>
+                  <Form.Select {...register('lang')}>
+                    <option value='es'>Español</option>
+                    <option value='en'>English</option>
+                  </Form.Select>
+                </Form.Group>
 
-              <div>
-                <label>
-                  Idioma <small className='text-secondary'>opcional</small>
-                </label>
-                <select {...register('lang')}>
-                  <option value='es'>Español</option>
-                  <option value='en'>English</option>
-                </select>
+                <div className='col-6 align-self-end text-center'>
+                  <span className=''>¿Ya estás registrado?</span><br/>
+                  <Link to='/login'>Iniciar sesión</Link>
+                </div>
               </div>
+            </div>
 
-              <input type='submit' />
-            </form>
-          </section>
-        </div>
-      </div>
-    </div>
+            <Button variant='primary' type='submit' className='mt-3'>
+              Registrarme
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 }
